029371d6b6165e03ee39f0ca3f5d8b62c8b94565
Serge Herkül <s.herkyl@gmail.com>
2015-03-06 09:10:19 +0200

    Fixed async

--- src/foreman_runner.js
+++ src/foreman_runner.js
@@ -3,11 +3,16 @@ var exec = require('child_process').exec;
 var sys = require('sys');
 
 exports.run = function (procfileLines, callback) {
-    var file = fs.createWriteStream('Procfile');
+  try {
+    var procfileLocation = process.cwd() + '/Procfile';
+    var file = fs.createWriteStream(procfileLocation);
     file.on('error', callback);
     file.on('close', callback);
     procfileLines.forEach(function(line) {
-        file.write(line.name + ':' + line.command + '\n');
+      file.write(line.name + ':' + line.command + '\n');
     });
     file.end();
+  } catch (error) {
+    callback(error);
+  }
 }
--- src/procfile_reader.js
+++ src/procfile_reader.js
@@ -2,27 +2,28 @@ var fs = require('fs');
 var readline = require('readline');
 var colors = require('colors/safe');
 
-exports.read = function (location, callback) {
-    console.log(colors.green('Reading from file ' + location));
-    var fs = require('fs'),
-        readline = require('readline'),
-        array = [];
+exports.read = function (fileName, callback) {
+  var location = process.cwd() + '/' + fileName;
+  console.log(colors.green('Reading from file ' + location));
+  var fs = require('fs')
+    , readline = require('readline')
+    , array = [];
 
-    var rd = readline.createInterface({
-        input: fs.createReadStream(location),
-        output: process.stdout,
-        terminal: false
-    });
+  var rd = readline.createInterface({
+    input: fs.createReadStream(location),
+    output: process.stdout,
+    terminal: false
+  });
 
-    rd.on('line', function(line) {
-        var pair = line.split(':');
-        array.push({
-            name: pair[0].trim(),
-            command: pair[1].trim()
-        });
+  rd.on('line', function(line) {
+    var pair = line.split(':');
+    array.push({
+      name: pair[0].trim(),
+      command: pair[1].trim()
     });
+  });
 
-    rd.on('close', function () {
-        callback(null, array);
-    });
+  rd.on('close', function () {
+    callback(null, array);
+  });
 }
